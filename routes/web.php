955ce43d3115be49f3c51c5397ba51e249c3f450
PietroFiori91 <pederflowers@hotmail.it>
2023-09-25 16:45:43 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\View;
+
+Route::get('/', function () {
+$comics = Config::get('comics');
+return View::make('comics.index', compact('comics'));
+});
